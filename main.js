d7a390c47061ac01cfdfb4e2e7012c5e26c2c328
Vincent <vinni.xander@gmail.com>
2022-07-13 16:59:46 -0600

    Create error for non matching passwords
    Generated in JS file. Also added function that prevents form
    submission if error is present.

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let password = document.querySelector('#psw');
 let passwordC = document.querySelector("#pswC");
-let error = document.querySelector ('#psw_match');
-
-passwordC.addEventListener('input', compareFields)
 
 function compareFields() {
-  let password = document.querySelector('#psw');
-  let passwordC = document.querySelector("#pswC");
-  if (password === passwordC) {
-    error.style.display = "none";
-  } else {
-    error.style.display = "block";
+  let password = document.querySelector('#psw').value;
+  let passwordC = document.querySelector("#pswC").value;
+  let error = document.querySelector ('#psw_error');
+  error.textContent = ""
+  if (password !== passwordC) {
+    error.textContent = "* Passwords do not match";
   }
 }
+
+passwordC.addEventListener('input', compareFields)
+password.addEventListener('input', compareFields)
+
+document.querySelector("#create").addEventListener("click", function(event) {
+  let error = document.querySelector ('#psw_error');
+  if (error.textContent == "* Passwords do not match") {
+  event.preventDefault();
+  }}
+);
